52f10519c809e4f5bc50331486efd5aeb0e90a57
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-06-21 23:17:11 -0700

    modified portfolio, css

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -38,12 +38,16 @@ export default function Portfolio ({setScreen}){
         <div id="horeison" className="work-horeison">
             <a href="https://josiesavill.github.io/horses/">
                 <div>
-                    <h3>Horses</h3>
+                    <h3>Horses In America</h3>
                 <div className="bgBox" style={{backgroundImage: `url(${img6})`}}></div>
                    
                 </div>
             </a>
-            <a href="https://github.com/JosieSavill/horses" style={{ fontSize: "20px" }}>View Repository</a> 
+            <a href="https://github.com/JosieSavill/horses" className="viewrepository">View Repository</a> 
+            <p style={{ fontSize: "20px", color: "black" }}>    
+                        React, HTML, JavaScript, CSS, Pexels
+
+                    </p>   
         </div>
 
         <div id="horeison" className="work-horeison">
@@ -53,7 +57,13 @@ export default function Portfolio ({setScreen}){
                     <div className="bgBox" style={{backgroundImage: `url(${img7})`}}></div>
                 </div>
             </a>
-            <a href="https://github.com/LL8719/ShareMeal" style={{ fontSize: "20px" }}>View Repository</a>
+            <a href="https://github.com/LL8719/ShareMeal" className="viewrepository">Group Project-View Repository</a>
+                    
+                    <p style={{ fontSize: "20px", color: "black" }}>    
+                        Styled Header/Footer/Navbar using HTML, JavaScript, CSS, Pexels, Chakra
+
+                    </p> 
+
         </div>
 
         {/* <div id="horeison" className="work-horeison">
@@ -63,7 +73,7 @@ export default function Portfolio ({setScreen}){
                     <div className="bgBox" style={{backgroundImage: `url(${img5})`}}></div>
                 </div>
             </a>
-            <a href="https://github.com/JosieSavill/space-weather-blog" style={{ fontSize: "20px" }}>View Repository</a>
+            <a href="https://github.com/JosieSavill/space-weather-blog" className="viewrepository">View Repository</a>
         </div> */}
 
         <div id="horeison" className="work-horeison">
@@ -73,39 +83,43 @@ export default function Portfolio ({setScreen}){
                     <div className="bgBox" style={{backgroundImage: `url(${img1})`}}></div>
                 </div>
             </a>
-            <a href="https://github.com/JosieSavill/beagzcal" style={{ fontSize: "20px" }}>View Repository</a>
+            <a href="https://github.com/JosieSavill/beagzcal" className="viewrepository">View Repository</a>
+            <p style={{ fontSize: "20px", color: "black" }}>    
+                        React
+
+                    </p> 
         </div>
 
-        <div id="horeison" className="work-horeison">
+        {/* <div id="horeison" className="work-horeison">
             <a href="https://josiesavill.github.io/gem/">
                 <div>
                     <h3>Gem</h3>
                     <div className="bgBox" style={{backgroundImage: `url(${img4})`}}></div>
                 </div>
             </a>
-            <a href="https://github.com/JosieSavill/gem" style={{ fontSize: "20px" }}>View Repository</a>
-        </div>
+            <a href="https://github.com/JosieSavill/gem" className="viewrepository">View Repository</a>
+        </div> */}
 
 
-        <div id="horeison" className="work-horeison">
+        {/* <div id="horeison" className="work-horeison">
             <a href="https://josiesavill.github.io/campeones/">
                 <div>
                     <h3>Campeones</h3>
                     <div className="bgBox" style={{backgroundImage: `url(${img3})`}}></div>
                 </div>
             </a>
-            <a href="https://github.com/JosieSavill/campeones" style={{ fontSize: "20px" }}>View Repository</a>
-        </div>
+            <a href="https://github.com/JosieSavill/campeones" className="viewrepository">View Repository</a>
+        </div> */}
 
-        <div id="horeison" className="work-horeison">
+        {/* <div id="horeison" className="work-horeison">
             <a href="https://josiesavill.github.io/HoriseonWebsite/">
                 <div>
                     <h3>Horeison Website</h3>
                     <div className="bgBox" style={{backgroundImage: `url(${img2})`}}></div>
                 </div>
             </a>
-            <a href="https://github.com/JosieSavill/HoriseonWebsite" style={{ fontSize: "20px" }}>View Repository</a>
-        </div>
+            <a href="https://github.com/JosieSavill/HoriseonWebsite" className="viewrepository">View Repository</a>
+        </div> */}
 
         
         
