cc412c94a8334959d2ad206d09b89690b991269b
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-07-13 22:23:58 -0700

    updated NewResume and css

--- src/components/NewResume.js
+++ src/components/NewResume.js
@@ -6,35 +6,62 @@ export default function NewResume (){
 
 
     return <div className="newResume">
-        Josie Savill
-Full Stack Developer
-Scottsdale, Arizona, United States
-
-Top Skills
-HTML, CSS/SCSS, JavaScript, Node/Express, REST, MySQL, MongoDB
-Libraries
-Chakra, Bootstrap, Skroller, Sequelize
-Frameworks
-React
-
-Languages
-English, Spanish
-Portfolio
-https://josiesavill.github.io/josie-savill-web-developer/
-Certifications
-University of Arizona, Web Development
-Work History
-Registered Respiratory Therapist 2006 thru current
-Providing comprehensive respiratory care to patients in adult, pediatric, neonatal populations; emergency department, intensive care settings; including initiation and monitoring of mechanical ventilation.
-
-Contact
-602.502.2022
-FiordaSavillJosie@gmail.com
-linkedin.com/in/josiefiordasavill
-
-       
-        
-    
+        <div className="resumeHeading">
+            Josie Savill
+            </div>
+        <div className="resume">
+            Full Stack Developer
+            Scottsdale, Arizona, United States
+            </div>
+        <div className="resumeBold">
+            Top Skills
+            </div>
+        <div className="resume">
+            HTML, CSS/SCSS, JavaScript, Node/Express, REST, MySQL, MongoDB
+            </div>
+        <div className="resumeBold">
+            Libraries
+            </div>
+        <div className="resume">
+            Chakra, Bootstrap, Skroller, Sequelize
+            </div>
+        <div className="resumeBold">
+            Frameworks
+            </div>
+        <div className="resume">
+            React
+            </div>
+        <div className="resumeBold">
+            Languages
+            </div>
+        <div className="resume">
+            English, Spanish
+            </div>
+        <div className="resumeBold">
+            Portfolio
+            </div>
+        {/* <link rel="portfolio" href="https://josiesavill.github.io/josie-savill-web-developer/">Josie Savill Web Developer</link> */}
+        <div className="resumeBold">
+            Certifications
+            </div>
+        <div className="resume">
+            University of Arizona, Full Stack Web Developer Bootcamp
+            </div>
+        <div  className="resumeBold">
+            Work History
+            </div>
+        <div className="resume">
+            Registered Respiratory Therapist 2006 thru current
+            Providing comprehensive respiratory care to patients in adult, pediatric, neonatal populations; emergency department, intensive care settings; including initiation and monitoring of mechanical ventilation.
+            </div>
+        <div  className="resumeBold">
+            Contact
+            </div>
+        <div className="resume">
+            602.502.2022
+            FiordaSavillJosie@gmail.com
+            linkedin.com/in/josiefiordasavill
+            </div>
         
         </div>
 }
