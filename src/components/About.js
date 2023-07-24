110d757742c0262bed11a1a95cb4f77c1ca20894
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-07-24 10:26:44 -0700

    modified About, index.css

--- src/components/About.js
+++ src/components/About.js
@@ -86,7 +86,8 @@ export default function About ({setScreen, screen}){
                     </div>
             </div>
         
-        <Footer />
+        {screen === "about" && <Footer />}
+        
         
     </div>
         
