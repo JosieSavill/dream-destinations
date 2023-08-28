08ae7587d4a50fb2f716cbd5f7d48fdef5b3ee15
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-08-27 19:27:47 -0700

    added embed link to vide

--- src/components/NewPortfolio.js
+++ src/components/NewPortfolio.js
@@ -34,6 +34,8 @@ export default function NewPortfolio (){
        </div>
 
        <div id="horeison" className="projects">
+
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/bAEprBbAfZM?si=z9aFMu5jN98C_kQ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <a href="https://youtu.be/bAEprBbAfZM">
                 <div>
                     <h3>Sunny Greece</h3>
