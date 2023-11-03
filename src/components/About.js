580a20164a814294b04a7733fd55e53d4fedbfae
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-11-03 15:14:13 -0700

    updated About js, index css

--- src/components/About.js
+++ src/components/About.js
@@ -17,52 +17,59 @@ export default function About ({setScreen, screen}){
     return <div>
         <Nav setScreen={setScreen}/>
         <div className="video-background">
-                <video autoPlay loop muted style={{width: "100%"}}>
-                    <source src={myVideo} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-            </div>
+            <video autoPlay loop muted style={{width: "100%"}}>
+                <source src={myVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+
+        <div className="main">
             <div className="row">
-                    <aside className="aside-contact">
+                <aside className="aside-contact">
                         
-                                <button className="portallink">
-                                    <ul><li><a onClick={()=>setScreen("about")}>Home</a></li></ul>
-                                </button>
+                    <button className="portallink">
+                        <ul><li><a onClick={()=>setScreen("about")}>Home</a></li></ul>
+                    </button>
 
-                                <button className="portallink">
-                                    <ul><li><a onClick={()=>setScreen("resume")} >About</a></li></ul>
-                                </button>
+                    <button className="portallink">
+                        <ul><li><a onClick={()=>setScreen("resume")} >About</a></li></ul>
+                    </button>
 
-                                <button className="portallink">
-                                    
-                                    <ul><li><a onClick={()=>setScreen("portfolio")}>Listings</a></li></ul>
-                                </button>
+                    <button className="portallink">
+                        <ul><li><a onClick={()=>setScreen("portfolio")}>America</a></li></ul>
+                    </button>
 
-                                <div className="footer-container">
-                
-                                    {screen === "about" && <Footer type="desktop"/>}
+                    <button className="portallink">
+                        <ul><li><a onClick={()=>setScreen("portfolio")}>Europe</a></li></ul>
+                    </button>
 
-                                </div>
+                    <button className="portallink">
+                        <ul><li><a onClick={()=>setScreen("portfolio")}>Mediterranean</a></li></ul>
+                    </button>
 
-                    </aside>
+                </aside>
                     {screen === "about" && <Footer type="mobile"/>}
                     <div className="feature">
                         {screen === "portfolio" && <NewPortfolio/>  }
                         {screen === "resume" && <Resume/>  }
                     </div>
-            </div>
-
-            <div className="logo">DREAM DESTINATIONS{' '}<br></br>
-            <span className="logo-description">The World is your Oyster</span></div>
-            {/* <div className="footer-container">
                 
-                {screen === "about" && <Footer type="desktop"/>}
 
-            </div> */}
-            
-      
-        
-        
+                <div className="logo">DREAM DESTINATIONS{' '}<br></br>
+                <span className="logo-description">The World is your Oyster</span></div>
+
+                <div className="footer-container">
+    
+                    {screen === "about" && <Footer type="desktop"/>}
+
+                </div>
+                {/* <div className="footer-container">
+                    
+                    {screen === "about" && <Footer type="desktop"/>}
+
+                </div> */}
+            </div>
+        </div>
     </div>
         
 }
