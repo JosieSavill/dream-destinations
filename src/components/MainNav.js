af60ba9499ce242d85760d0b016b65f98c510153
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-13 10:53:40 -0700

    updated ManNaav, styles

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,37 +1,36 @@
 export default function MainNav({setScreen}){
   return <>
    <button className="portallink">
-                        <ul><li><a onClick={()=>setScreen("home")}>Home</a></li></ul>
-                    </button>
+        <ul><li><a onClick={()=>setScreen("home")}>Home</a></li></ul>
+    </button>
 
-                    <button className="portallink">
-                        <ul><li><a onClick={()=>setScreen("resume")}>About</a></li></ul>
-                    </button>
+    <button className="portallink">
+        <ul><li><a onClick={()=>setScreen("resume")}>About</a></li></ul>
+    </button>
 
-                    <button className="portallink">
-                        <ul><li><a onClick={()=>setScreen("north")}>North America</a></li></ul>
-                    </button>
+    <button className="portallink">
+        <ul><li><a onClick={()=>setScreen("north")}>North America</a></li></ul>
+    </button>
 
-                    <button className="portallink">
-                        <ul><li><a onClick={()=>setScreen("south")}>South America</a></li></ul>
-                    </button>
+    <button className="portallink">
+        <ul><li><a onClick={()=>setScreen("south")}>South America</a></li></ul>
+    </button>
 
-                    <button className="portallink">
-                        <ul><li><a onClick={()=>setScreen("europe")}>Europe</a></li></ul>
-                    </button>
+    <button className="portallink">
+        <ul><li><a onClick={()=>setScreen("europe")}>Europe</a></li></ul>
+    </button>
 
-                    <button className="portallink">
-                        <ul><li><a onClick={()=>setScreen("asia")}>Asia</a></li></ul>
-                    </button>
-
-                    
-                    <button className="portallink createAccount" style={{ border: '2px solid white' }}>
-                        <ul>
-                            <li>
-                            <a onClick={() => setScreen("portfolio")}>Create Account</a>
-                            </li>
-                        </ul>
-                    </button>
+    <button className="portallink">
+        <ul><li><a onClick={()=>setScreen("asia")}>Asia</a></li></ul>
+    </button>
+    
+    <button className="createAccount">
+        <ul>
+            <li>
+            <a onClick={() => setScreen("portfolio")}>Create Account</a>
+            </li>
+        </ul>
+    </button>
 
   </>
 }
