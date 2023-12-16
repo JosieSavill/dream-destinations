789d3f51f485976ada87410e773ad77a52ff1005
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-16 12:48:56 -0700

    pdated MobileNav, mobileNav, nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -6,13 +6,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 export default function MobileNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Container className='mobile-nav'>
+        <Navbar.Brand href="#home">Sign In</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">Reservation</Nav.Link>
+            <Nav.Link href="#link">Listings</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
