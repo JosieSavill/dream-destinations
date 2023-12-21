f9a2190a1faff742f813fab63a371ab5bb5931d8
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-21 16:49:34 -0700

    updated MobileNav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -2,16 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import oyster from "../assets/oyster-tan-gray.gif";
 
 export default function MobileNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container className='mobile-nav'>
-        <Navbar.Brand href="#home">Sign In</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <span className="oyster-logo">
+            <img src={oyster} width={45} height={30}/>
+          </span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Reservation</Nav.Link>
+            <Nav.Link href="#home">Sign-in</Nav.Link>
             <Nav.Link href="#link">Listings</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
