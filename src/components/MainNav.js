73850af8f550f5b20e85fddf8b5a2dbff37733f5
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-21 16:10:56 -0700

    updated MainNav, nav styles

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,19 +1,19 @@
-export default function MainNav({setScreen}){
-  return <>
+// export default function MainNav({setScreen}){
+//   return <>
    
-        <ul><li><a onClick={()=>setScreen("home")}>Home</a></li></ul>
+//         <ul><li><a onClick={()=>setScreen("home")}>Home</a></li></ul>
     
-        <ul><li><a onClick={()=>setScreen("resume")}>About</a></li></ul>
+//         <ul><li><a onClick={()=>setScreen("resume")}>About</a></li></ul>
     
-        <ul><li><a onClick={()=>setScreen("north")}>North America</a></li></ul>
+//         <ul><li><a onClick={()=>setScreen("north")}>North America</a></li></ul>
     
-        <ul><li><a onClick={()=>setScreen("south")}>South America</a></li></ul>
+//         <ul><li><a onClick={()=>setScreen("south")}>South America</a></li></ul>
     
-        <ul><li><a onClick={()=>setScreen("europe")}>Europe</a></li></ul>
+//         <ul><li><a onClick={()=>setScreen("europe")}>Europe</a></li></ul>
     
-        <ul><li><a onClick={()=>setScreen("asia")}>Asia</a></li></ul>
+//         <ul><li><a onClick={()=>setScreen("asia")}>Asia</a></li></ul>
     
-        <ul><li className="createAccount"><a onClick={() => setScreen("portfolio")}>Create Account</a></li></ul>
+//         <ul><li className="createAccount"><a onClick={() => setScreen("portfolio")}>Create Account</a></li></ul>
     
-  </>
-}
+//   </>
+// }
