ff48d18f669e1090d1e9ac282c6f1af73d64d588
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-22 12:24:00 -0700

    updated createAccount, mobileNav, nav, MobileNav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -3,21 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import oyster from "../assets/oyster-tan-gray.gif";
+import { BsList } from "react-icons/bs";
 
 export default function MobileNav() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className='mobile-nav'>
-        <Navbar.Brand href="#home">
-          <span className="oyster-logo">
-            <img src={oyster} width={45} height={30}/>
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar expand="lg" className="mobile-nav">
+      <Container className=''>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+            <BsList style={{color: "white", fontSize: "20px"}}/>
+          </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Sign-in</Nav.Link>
+          <Nav>
+            <Nav.Link href="#home">Reservation</Nav.Link>
             <Nav.Link href="#link">Listings</Nav.Link>
+            <Nav.Link href="#link">Sign</Nav.Link>
             <NavDropdown title="More..." id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
