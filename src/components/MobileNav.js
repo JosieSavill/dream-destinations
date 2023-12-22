2c4b92dc78be6202bc208bf03d94a8182b1829b2
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-22 12:34:54 -0700

    updated MobileNav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -17,7 +17,7 @@ export default function MobileNav() {
           <Nav>
             <Nav.Link href="#home">Reservation</Nav.Link>
             <Nav.Link href="#link">Listings</Nav.Link>
-            <Nav.Link href="#link">Sign</Nav.Link>
+            <Nav.Link href="#link">Sign In</Nav.Link>
             <NavDropdown title="More..." id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
