d9998043bb8b01a025e70f18b277ab8dd78d6266
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-12-23 16:48:15 -0700

    updated MobileNav, styles

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -8,25 +8,25 @@ import { BsList } from "react-icons/bs";
 export default function MobileNav() {
   return (
     <Navbar expand="lg" className="mobile-nav">
-      <Container className=''>
+      <Container className='mobile-nav-container'>
         
         <Navbar.Toggle aria-controls="basic-navbar-nav" >
-            <BsList style={{color: "white", fontSize: "20px"}}/>
+            <BsList style={{color: "white", fontSize: "30px"}}/>
           </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <Nav.Link href="#home">Reservation</Nav.Link>
+            <Nav.Link href="#home">Reservations</Nav.Link>
             <Nav.Link href="#link">Listings</Nav.Link>
             <Nav.Link href="#link">Sign In</Nav.Link>
-            <NavDropdown title="More..." id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown title="More..." id="basic-nav-dropdown" className='dropdown-nav'>
+              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">North America</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">South America</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Europe</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Asia</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Create Account
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
