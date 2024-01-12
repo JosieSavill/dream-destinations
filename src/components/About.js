a5a4d72ee944ed24c89d8b1819e64c9009f7c613
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2024-01-11 18:52:50 -0700

    updated about stylesheet

--- src/components/About.js
+++ src/components/About.js
@@ -8,27 +8,19 @@ import tanOyster from "../assets/oyster-tan-gray.gif";
 export default function About (){
 
 
-    return <div className="newResume">
+    return <div className="about">
 
-        <div className="resumeContainer">
+        <div className="about-container">
 
            
 
-            <div className="resumeHeading">
+            <div className="about-heading">
                 DREAM DESTINATIONS
             </div>
 
             <div className="img">
-
-                {/* <img src={temple} className="profile-pic" alt="chinese temple img"/> */}
-
-                {/* <img src={iceland} className="profile-pic" alt="iceland aurora borealis img"/> */}
-
-                <img src={greece} className="profile-pic" alt="greece white domed building img"/>
-
+                <img src={greece} className="destination-pic" alt="greece white domed building img"/>
             </div>
-
-            
 
             <div className="goals">  
                 <div> 
@@ -40,44 +32,32 @@ export default function About (){
             </div> 
 
             <div className="img">
-
-                <img src={temple} className="profile-pic" alt="chinese temple img"/>
-
-                {/* <img src={iceland} className="profile-pic" alt="iceland aurora borealis img"/> */}
-
-                {/* <img src={greece} className="profile-pic" alt="greece white domed building img"/> */}
-
+                <img src={temple} className="destination-pic" alt="chinese temple img"/>
             </div>   
            
-            <div className="resumeBold">
+            <div className="about-bold">
                 History
                 </div>
-            <div className="resume">
+            <div className="about-text">
                 Our company has been providing services globally for a 20 years.
             </div>
 
             <div className="img">
-
-                {/* <img src={temple} className="profile-pic" alt="chinese temple img"/> */}
-
-                <img src={iceland} className="profile-pic" alt="iceland aurora borealis img"/>
-
-                {/* <img src={greece} className="profile-pic" alt="greece white domed building img"/> */}
-
+                <img src={iceland} className="destination-pic" alt="iceland aurora borealis img"/>
             </div>
             
-            <div className="resumeBold">
+            <div className="about-bold">
                 Location
                 </div>
-            <div className="resume">
+            <div className="about-text">
                 Our offices are headquartered in beautiful New York, NY, United States.
                 </div>
         
-            <div className="resume">
+            <div className="about-text">
                 <a className='gmail' href="https://1drv.ms/w/s!AhfCqpI38Ls5g-NDcHm7ATLHYItS1A?e=RlHTo5"><BsArrowRightShort/>Download Pricing PDF</a>
                 </div>
 
-            <div className="resumeIntroduction">
+            <div className="about-introduction">
                 The World Is Your Oyster 
             </div>    
 
@@ -85,10 +65,10 @@ export default function About (){
                 <img src={tanOyster} width={129} height={76}/>
             </span>
 
-            <div  className="resumeBold">
+            <div  className="about-bold">
                 Contact
                 </div>
-            <div className="resume-email">
+            <div className="about-email">
                 1.888.888.2023<br></br>
                 <a className='gmail' href="mailto:FiordaSavillJosie@gmail.com"><BsArrowRightShort/>DreamDestinations@dream.com</a>
                 </div>
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,14 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import TopNav from "./TopNav";
-import MainNav from "./MainNav";
-import CreateAccount from './CreateAccount';
-import About from './About';
+// import TopNav from "./TopNav";
+// import MainNav from "./MainNav";
+// import CreateAccount from './CreateAccount';
+// import About from './About';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-// import oyster from "../assets/oyster-tan-gray.gif";
 import { BsList } from "react-icons/bs";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
 export default function MobileNav({setScreen}) {
